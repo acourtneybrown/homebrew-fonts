44574b2ae83c3a1f3f45a98e06f55930f51b3f33
Adam Brown <acourtneybrown@gmail.com>
2020-06-18 18:21:57 -0700

    First pass at cask file for Kernit font

---
+++ Casks/font-kernit-font.rb
@@ -0,0 +1,11 @@
+cask 'font-3270-nerd-font' do
+  version '1.0.0'
+  sha256 '2619ed0ce666d716cf627ac1abf688c1a4da8f326837308c448423694f434b84'
+
+  url "https://justkernit.com/Kernit.zip"
+  name 'Kernit'
+  homepage 'https://justkernit.com'
+
+  font 'ZIP FILE CONTENT/OTF/Kernit-Filled.otf'
+  font 'ZIP FILE CONTENT/OTF/Kernit-Outline.otf'
+end
