d4c22b418be7d36f0b11c737f35d684b4588bf72
Adam Brown <acourtneybrown@gmail.com>
2020-11-19 13:10:24 -0700

    Add Gerry (https://uglygerry.com/) font

---
+++ Casks/font-gerry.rb
@@ -0,0 +1,10 @@
+cask 'font-gerry' do
+  version '1.0.0'
+  sha256 '38332905de7a8bcbda48038edd7cc5dfd26924b3991eebfc9f8ca84b0b35f1e4'
+
+  url "https://uglygerry.com/_res/fonts/Gerry.otf"
+  name 'Gerry'
+  homepage 'https://uglygerry.com/'
+
+  font 'Gerry.otf'
+end
