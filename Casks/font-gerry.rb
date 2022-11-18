66564c31de5bd6584cc5d1dcd17fc8ceaad98419
Adam Brown <acourtneybrown@gmail.com>
2022-11-17 19:15:04 -0700

    Update url for Gerry font

--- Casks/font-gerry.rb
+++ Casks/font-gerry.rb
@@ -2,7 +2,7 @@ cask "font-gerry" do
   version "1.0.0"
   sha256 "38332905de7a8bcbda48038edd7cc5dfd26924b3991eebfc9f8ca84b0b35f1e4"
 
-  url "https://uglygerry.com/_res/fonts/Gerry.otf"
+  url "https://github.com/acourtneybrown/homebrew-fonts/raw/75eb92a7306568c9d60f1bfe1af917840146cc59/fonts/Gerry.otf"
   name "Gerry"
   homepage "https://uglygerry.com/"
 
