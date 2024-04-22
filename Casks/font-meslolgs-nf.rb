28fb04fc059999dcf05a29fc0af8e1b852816494
Adam Brown <acourtneybrown@gmail.com>
2024-04-22 11:07:18 -0700

    Fix errors from `brew test-bot`

--- Casks/font-meslolgs-nf.rb
+++ Casks/font-meslolgs-nf.rb
@@ -2,8 +2,7 @@ cask "font-meslolgs-nf" do
   version "2.3.3"
   sha256 "54935f4c429506b135f8a0dee81c924556e7ccdb5b8eca254c334c420c92d454"
 
-  url "https://github.com/romkatv/powerlevel10k-media/archive/refs/tags/v2.3.3.zip",
-      verified: "github.com/romkatv/powerlevel10k-media/"
+  url "https://github.com/romkatv/powerlevel10k-media/archive/refs/tags/v#{version}.zip"
   name "MesloLGS NF"
   homepage "https://github.com/romkatv/powerlevel10k/blob/master/font.md#manual-font-installation"
 
