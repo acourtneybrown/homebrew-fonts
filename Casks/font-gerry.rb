93a55362463f3600a513f749576e231d6c6ba6ca
Adam Brown <acourtneybrown@gmail.com>
2024-04-22 10:37:08 -0700

    Fix style errors

--- Casks/font-gerry.rb
+++ Casks/font-gerry.rb
@@ -3,7 +3,7 @@ cask "font-gerry" do
   sha256 :no_check
 
   url "https://github.com/acourtneybrown/homebrew-fonts/raw/75eb92a7306568c9d60f1bfe1af917840146cc59/fonts/Gerry.otf",
-      verified: "github.com/acourtneybrown/homebrew-fonts"
+      verified: "github.com/acourtneybrown/homebrew-fonts/"
   name "Gerry"
   homepage "https://uglygerry.com/"
 
